89d36a5081dec807abef2dbf7e62b7d2600c9d53
なめこ <nameko@nebuko-no-MacBook-Air.local>
2014-11-09 22:39:46 +0900

    Update-README, Upload SenbeiMakerGame

---
+++ SenbeiMakerGame.java
@@ -0,0 +1,49 @@
+import java.io.*;
+
+public class SenbeiMakerGame {
+	public static void main (String[] args) {
+		String baff = null;     //キーボードバッファ
+		SenbeiMaker maker = new SenbeiMaker(1);
+	try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		while (baff.equals("q")) {    //qキーの押下まで繰り返す
+			System.out.print("<");
+			baff = in.readLine();     //キーボード入力の読み込み
+			if (baff.equals("make")) {   //make処理を書く
+				
+			}
+			else if (baff.equals("item")) {   //アイテム作成処理
+				System.out.println("Item select");
+				System.out.println("1:HAND 2:JIJI 3:FARM 4:FACTORY 5:MINE");
+				System.out.print("<");
+				baff = in.readLine();    //キーボード入力の読み込み
+				
+				//アイテム作成処理
+			}
+			else if (baff.equals("pwup")) {
+				System.out.println("PowerupItem select");
+				System.out.println("1:HAND 2:JIJI 3:FARM 4:FACTORY 5:MINE");
+				System.out.print("<");
+				baff = in.readLine();    //キーボード入力の読み込み
+				
+				//パワーアップ処理
+				
+			}
+			else if (baff.equals("show")) {    //showの処理
+				
+			}
+			else {   //入力が不正なとき
+				System.out.println("そんなコマンドないよ");
+			}
+			
+		}
+				
+			
+	} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		
+	}
+
+}
